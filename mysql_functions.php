fcf1f575b26d15341e4d81ccb48cf93b7ea5df78
Mark Andrews <mandrews18@knights.ucf.edu>
2015-04-09 12:48:34 -0400

    created function to output all events for uid or rid
    Can't test now so probably doesn't work but should create a table consisting of events from a group or university as well as related groups or universities.

--- mysql_functions.php
+++ mysql_functions.php
@@ -40,4 +40,52 @@ function getAdminInfo($rso_id, $email, $db){
 
 	}
 ?>
+
+	<?php 
+	function printEventList2($rid, $uid, $db){
+		if($rid == NULL){
+			$temp = $db->query("SELECT * FROM university_event_list
+			LEFT JOIN event ON university_event_list.eid = event.eid
+			WHERE (university_event_list.uid) = '" . $uid . "'
+			UNION
+			SELECT * FROM rso_event_list
+			LEFT JOIN event ON rso_event_list.eid = event.eid
+			WHERE (rso_event_list.rid) = (
+				SELECT rid FROM university_rso_link
+				WHERE uid = '" . $uid . "')
+			");
+			
+		} else if ($uid == NULL){
+			$temp = $db->query("SELECT * FROM rso_event_list
+			LEFT JOIN event ON rso_event_list.eid = event.eid
+			WHERE (rso_event_list.rid) = '" . $rid . "'
+			UNION
+			SELECT * FROM university_event_list
+			LEFT JOIN event ON university_event_list.eid = event.eid
+			WHERE (university_event_list.uid) = (
+				SELECT uid FROM university_rso_link
+				WHERE rid = '" . $rid . "')
+			");
+			
+		}
+	
+		$event = $temp->fetch_all(MYSQLI_ASSOC);
+		
+		foreach($event as $e){
+			$description = $e['description'];
+			if(count($description) > 200 ){
+				$description = substr($description, 0, 200) . '...';
+			}
+			?>
+
+			<div id="event_block">
+				<h3><a href="event.php?event=<?php echo escape($e['eid']); ?>"><?php echo escape($e['name']); ?></h3></a>
+				<?php echo escape($e['date']); ?> at <?php echo escape($e['time']); ?> <br><br>
+				<p><?php echo escape($description); ?></p>
+			</div>
+			<?php
+		}
+
+	}
+?>
 </html>
