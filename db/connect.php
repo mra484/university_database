b457ec409e5cc305dbf1651f51e6c7230702c9e8
kaveman1105 <kaveman1105@gmail.com>
2015-04-15 07:45:15 -0400

    copying into kevin branch

--- db/connect.php
+++ db/connect.php
@@ -2,7 +2,7 @@
 $db = new mysqli('127.0.0.1', 'root', '', 'cop4710_app');
 
 if($db->connect_errno){
-	//echo $db->connect_errno;
-	//echo $db->connect_error;
+	echo $db->connect_errno;
+	echo $db->connect_error;
 	die('Sorry, we are having some problems.');
 }
